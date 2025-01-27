070a007c580d944a4d46e5d40a9d8684aa597b51
Sok Kim Thanh <thanhsk1991@gmail.com>
2025-01-27 17:38:51 +0700

    Vẽ báo cáo

--- GitLogAggregator/Reports/CrystalReport1.cs
+++ GitLogAggregator/Reports/CrystalReport1.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace GitLogAggregator {
+namespace GitLogAggregator.Reports {
     using System;
     using System.ComponentModel;
     using CrystalDecisions.Shared;
@@ -41,7 +41,7 @@ namespace GitLogAggregator {
         
         public override string FullResourceName {
             get {
-                return "GitLogAggregator.CrystalReport1.rpt";
+                return "GitLogAggregator.Reports.CrystalReport1.rpt";
             }
             set {
                 // Do nothing
